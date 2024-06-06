fe47be559e51650fecb4feacae11cbba6c7da9cf
Deven Caron <devencaron@gmail.com>
2024-06-06 16:15:52 -0400

    Image component progress

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -3,3 +3,11 @@ import { Scroll } from './classes/Scroll';
 
 new Transitions();
 Scroll.init();
+
+new PerformanceObserver((entryList) => {
+    for (const entry of entryList.getEntries()) {
+      console.log('Layout shift:', entry);
+    }
+}).observe({type: 'layout-shift', buffered: true});
+
+console.log('App is running');
