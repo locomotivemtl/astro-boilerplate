dbd4c687cee1a32e214212ed241547b05b701c6d
Arnaud Pinot <pinot.arnaud@gmail.com>
2024-07-25 14:17:51 -0400

    Astro SEO (#12)
    * Add astro seo plugin
    
    * Fix package-lock.json
    
    ---------
    
    Co-authored-by: Deven Caron <devencaron@gmail.com>

---
+++ src/data/seo.ts
@@ -0,0 +1,27 @@
+import metaImage from '@images/meta.png';
+
+export const defaultSeo = {
+    title: 'Astro boilerplate',
+    description: 'Boilerplate for Astro',
+    social: {
+        facebook: {
+            title: 'Astro boilerplate',
+            image: {
+                url: metaImage.src
+            },
+            description: 'Boilerplate for Astro'
+        },
+        twitter: {
+            creator: '@LocomotiveMTL',
+            title: 'Astro boilerplate',
+            image: {
+                url: metaImage.src
+            },
+            description: 'Boilerplate for Astro'
+        }
+    },
+    advanced: {
+        robots: ['noindex', 'nofollow'],
+        canonical: 'https://locomotive.ca'
+    }
+};
