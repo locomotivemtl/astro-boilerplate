ee3b0bf58099b418a5f60687cca63b3adee7a041
Deven Caron <devencaron@gmail.com>
2024-10-02 17:10:50 -0400

    Add i18n routing

--- astro.config.ts
+++ astro.config.ts
@@ -3,12 +3,23 @@ import tailwind from '@astrojs/tailwind';
 import svgSprite from 'astro-svg-sprite';
 import tailwindConfig from './tailwind.config';
 import postcssTailwindShortcuts from '@locomotivemtl/postcss-tailwind-shortcuts';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 const isProd = import.meta.env.PROD;
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://locomotive-astro-boilerplate.vercel.app',
+    output: 'hybrid',
+    adapter: vercelServerless(),
+    i18n: {
+        defaultLocale: 'fr',
+        locales: ['fr', 'en'],
+        routing: {
+            prefixDefaultLocale: false,
+            fallbackType: 'redirect'
+        }
+    },
     vite: {
         css: {
             preprocessorOptions: {
