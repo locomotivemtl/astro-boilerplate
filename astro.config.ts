5b97505a81d937083f4786ef9b1c4aeee2b1a5c8
Deven Caron <devencaron@gmail.com>
2024-10-08 14:50:48 -0400

    Format

--- astro.config.ts
+++ astro.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
                     postcssTailwindShortcuts(tailwindConfig.theme),
                     postcssNested,
                     postcssSimpleVars(),
-                    postcssMixins,
+                    postcssMixins
                 ]
             }
         },
