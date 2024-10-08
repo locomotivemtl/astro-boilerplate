ffb3b4510e83687ce59a0ec1866945ede1d80b8a
Deven Caron <devencaron@gmail.com>
2024-10-08 14:50:40 -0400

    Prepare scss removal

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import svgSprite from 'astro-svg-sprite';
 import tailwindConfig from './tailwind.config';
 import postcssTailwindShortcuts from '@locomotivemtl/postcss-tailwind-shortcuts';
+import postcssNested from 'postcss-nested';
+import postcssSimpleVars from 'postcss-simple-vars';
+import postcssMixins from 'postcss-mixins';
 
 const isProd = import.meta.env.PROD;
 
@@ -11,18 +14,13 @@ export default defineConfig({
     site: 'https://locomotive-astro-boilerplate.vercel.app',
     vite: {
         css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: `
-                        @use "sass:math";
-                        @use "sass:list";
-                        @use "@styles/tools/maths" as *;
-                        @use "@styles/tools/functions" as *;
-                    `
-                }
-            },
             postcss: {
-                plugins: [postcssTailwindShortcuts(tailwindConfig.theme)]
+                plugins: [
+                    postcssTailwindShortcuts(tailwindConfig.theme),
+                    postcssNested,
+                    postcssSimpleVars(),
+                    postcssMixins,
+                ]
             }
         },
         esbuild: {
