c9b549d8d01269641e2e10839cb517c9bbe814ef
Arnaud Pinot <pinot.arnaud@gmail.com>
2025-03-20 16:26:27 -0400

    Enhancing Component Management & Built-in Element Extension Support (#44)
    * Refactor and improve component manager + Update documentation
    
    * Add className argument to componentElement to specify namespace
    
    * Update src/scripts/stores/README.md
    
    Co-authored-by: Deven Caron <devencaron@gmail.com>
    
    ---------
    
    Co-authored-by: Deven Caron <devencaron@gmail.com>

--- src/scripts/stores/componentManager.ts
+++ src/scripts/stores/componentManager.ts
@@ -1,46 +1,46 @@
 import { atom } from 'nanostores';
 
-export class ComponentElement extends HTMLElement {
-    prototypeType: string;
+export const ComponentElement = <BaseClass extends CustomElementConstructor>(
+    Base: BaseClass,
+    className: string
+) => {
+    return class extends Base {
+        prototypeType: string;
 
-    constructor() {
-        super();
+        constructor(...args: any[]) {
+            super(...args);
 
-        this.prototypeType = this.constructor.name;
+            this.prototypeType = className;
 
-        if (this.id === '') {
-            const index = $componentsManagerIncrement.get() + 1;
-            $componentsManagerIncrement.set(index);
-            this.id = `${this.constructor.name.toLowerCase()}-${index}`;
+            if (!this.id) {
+                const index = $componentsManagerIncrement.get() + 1;
+                $componentsManagerIncrement.set(index);
+                this.id = `${this.prototypeType.toLowerCase()}-${index}`;
+            }
         }
-    }
 
-    // =============================================================================
-    // Lifecycle
-    // =============================================================================
-    connectedCallback() {
-        this.register();
-    }
+        connectedCallback() {
+            if (typeof (Base.prototype as any).connectedCallback === 'function') {
+                (Base.prototype as any).connectedCallback.call(this);
+            }
 
-    disconnectedCallback() {
-        this.unregister();
-    }
+            $componentsManager.set([...$componentsManager.get(), this]);
+        }
 
-    // =============================================================================
-    // Methods
-    // =============================================================================
-    register() {
-        $componentsManager.set([...$componentsManager.get(), this]);
-    }
-    unregister() {
-        $componentsManager.set(
-            $componentsManager.get().filter(($component) => $component.id !== this.id)
-        );
-    }
-}
+        disconnectedCallback() {
+            if (typeof (Base.prototype as any).disconnectedCallback === 'function') {
+                (Base.prototype as any).disconnectedCallback.call(this);
+            }
+
+            $componentsManager.set(
+                $componentsManager.get().filter(($component) => $component.id !== this.id)
+            );
+        }
+    };
+};
 
 export const $componentsManagerIncrement = atom<number>(0);
-export const $componentsManager = atom<ComponentElement[]>([]);
+export const $componentsManager = atom<InstanceType<ReturnType<typeof ComponentElement>>[]>([]);
 
 export const getComponentById = (id: string) => {
     return $componentsManager.get().find(($component) => $component.id === id);
@@ -48,7 +48,11 @@ export const getComponentById = (id: string) => {
 
 export const getComponentsByPrototype = (
     prototype: string,
-    selectorsToExclude: string[] | string | HTMLElement | ComponentElement = []
+    selectorsToExclude:
+        | string[]
+        | string
+        | HTMLElement
+        | InstanceType<ReturnType<typeof ComponentElement>> = []
 ) => {
     let excludedSelectors: string[] = [];
 
@@ -60,8 +64,8 @@ export const getComponentsByPrototype = (
         excludedSelectors = [`#${selectorsToExclude.id}`];
     }
 
-    return ($componentsManager.get() as ComponentElement[]).filter(
-        ($component: ComponentElement) => {
+    return ($componentsManager.get() as InstanceType<ReturnType<typeof ComponentElement>>[]).filter(
+        ($component: InstanceType<ReturnType<typeof ComponentElement>>) => {
             return (
                 prototype === $component.prototypeType &&
                 !excludedSelectors.some((selector: string) => $component.matches(selector))
