a88d5cb9d1490105f1ecbc145d92f9d17f479767
Deven Caron <devencaron@gmail.com>
2026-02-27 10:04:54 -0500

    Fix font api provider

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import icon from 'astro-icon';
 import postcssUtopia from 'postcss-utopia';
 import postcssHelpersFunctions from '@locomotivemtl/postcss-helpers-functions';
 import postcssTailwindShortcuts from '@locomotivemtl/postcss-tailwind-shortcuts';
 import tailwindcss from '@tailwindcss/postcss';
-
-const isProd = import.meta.env.PROD;
 
 // https://astro.build/config
 export default defineConfig({
@@ -37,24 +35,26 @@ export default defineConfig({
     experimental: {
         fonts: [
             {
-                provider: 'local',
+                provider: fontProviders.local(),
                 name: 'Source Sans Pro',
                 cssVariable: '--custom-font-sans',
                 fallbacks: ['sans-serif'],
-                variants: [
-                    {
-                        weight: 400,
-                        style: 'normal',
-                        display: 'swap',
-                        src: ['./src/assets/fonts/SourceSans3-Regular.woff2']
-                    },
-                    {
-                        weight: 700,
-                        style: 'normal',
-                        display: 'swap',
-                        src: ['./src/assets/fonts/SourceSans3-Bold.woff2']
-                    }
-                ]
+                options: {
+                    variants: [
+                        {
+                            weight: 400,
+                            style: 'normal',
+                            display: 'swap',
+                            src: ['./src/assets/fonts/SourceSans3-Regular.woff2']
+                        },
+                        {
+                            weight: 700,
+                            style: 'normal',
+                            display: 'swap',
+                            src: ['./src/assets/fonts/SourceSans3-Bold.woff2']
+                        }
+                    ]
+                }
             }
         ]
     }
